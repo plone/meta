e917c9a4dc4eb79550d86fd7a700a65a0131c719
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-01-23 23:52:25 +0100

    chore: more methods

--- config/config-package.py
+++ config/config-package.py
@@ -207,6 +207,24 @@ class PackageConfiguration:
 
         return destination.relative_to(self.path)
 
+    def remove_old_files(self):
+        filenames = ('bootstrap.py', '.travis.yml')
+        with change_dir(self.path):
+            for filename in filenames:
+                if pathlib.Path(filename).exists():
+                    call('git', 'rm', 'filename')
+
+    def remove_toml_empty_sections(self):
+        meta_cfg = {k: v for k, v in self.meta_cfg.items() if v}
+        with change_dir(self.path):
+            with open('.meta.toml', 'w') as meta_f:
+                meta_f.write(META_HINT.format(config_type=self.config_type))
+                meta_f.write('\n')
+                toml.dump(
+                    meta_cfg, meta_f,
+                    TomlArraySeparatorEncoderWithNewline(
+                        separator=',\n   ', indent_first_line=True))
+
     def configure(self):
         self._add_project_to_config_type_list()
 
@@ -219,21 +237,10 @@ class PackageConfiguration:
             self.tox(),
         ]
 
-        with change_dir(self.path) as cwd:
-            if pathlib.Path('bootstrap.py').exists():
-                call('git', 'rm', 'bootstrap.py')
-            if pathlib.Path('.travis.yml').exists():
-                call('git', 'rm', '.travis.yml')
-            # Remove empty sections:
-            meta_cfg = {k: v for k, v in self.meta_cfg.items() if v}
-            with open('.meta.toml', 'w') as meta_f:
-                meta_f.write(META_HINT.format(config_type=self.config_type))
-                meta_f.write('\n')
-                toml.dump(
-                    meta_cfg, meta_f,
-                    TomlArraySeparatorEncoderWithNewline(
-                        separator=',\n   ', indent_first_line=True))
+        self.remove_old_files()
+        self.remove_toml_empty_sections()
 
+        with change_dir(self.path) as cwd:
             tox_path = shutil.which('tox') or (
                 pathlib.Path(cwd) / 'bin' / 'tox')
             call(tox_path, '-p', 'auto')
