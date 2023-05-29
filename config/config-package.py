4f3e4fab1418f046cea3b0bf0c6ed6459395bd89
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-05-29 10:53:53 +0200

    chore: remove unused variable

--- config/config-package.py
+++ config/config-package.py
@@ -24,7 +24,6 @@ META_HINT_MARKDOWN = """\
 Generated from:
 https://github.com/plone/meta/tree/master/config/{config_type}
 --> """
-FUTURE_PYTHON_VERSION = "3.12.0-alpha.2"
 DEFAULT = object()
 
 
