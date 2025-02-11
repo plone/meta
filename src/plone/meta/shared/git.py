651ff765d841edcf648f7ca19cd992ea2235473f
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-02-11 10:23:07 +0100

    chore: black

--- src/plone/meta/shared/git.py
+++ src/plone/meta/shared/git.py
@@ -7,8 +7,8 @@ import pathlib
 def get_commit_id():
     """Return the first 8 digits of the commit id of this repository."""
     return call(
-        'git', 'rev-parse', '--short=8', 'HEAD',
-        capture_output=True).stdout.strip()
+        "git", "rev-parse", "--short=8", "HEAD", capture_output=True
+    ).stdout.strip()
 
 
 def get_branch_name(override, config_type):
@@ -19,13 +19,11 @@ def get_branch_name(override, config_type):
     if override == "current":
         # Note: can be empty if not on a branch.
         override = call(
-            'git', 'branch', '--show-current',
-            capture_output=True).stdout.splitlines()[0]
+            "git", "branch", "--show-current", capture_output=True
+        ).stdout.splitlines()[0]
 
     with change_dir(pathlib.Path(__file__).parent):
-        return (
-            override
-            or f"config-with-{config_type}-template-{get_commit_id()}")
+        return override or f"config-with-{config_type}-template-{get_commit_id()}"
 
 
 def git_branch(branch_name) -> bool:
@@ -34,18 +32,19 @@ def git_branch(branch_name) -> bool:
     Return `True` if updating.
     """
     branches = call(
-        'git', 'branch', '--format', '%(refname:short)',
-        capture_output=True).stdout.splitlines()
+        "git", "branch", "--format", "%(refname:short)", capture_output=True
+    ).stdout.splitlines()
     if branch_name in branches:
-        call('git', 'checkout', branch_name)
+        call("git", "checkout", branch_name)
         updating = True
     else:
-        call('git', 'checkout', '-b', branch_name)
+        call("git", "checkout", "-b", branch_name)
         updating = False
     return updating
 
+
 def git_server_url():
     """Return the repository URL"""
-    output = call('git', 'remote', 'get-url', 'origin', capture_output=True)
+    output = call("git", "remote", "get-url", "origin", capture_output=True)
     url = output.stdout.splitlines()[0]
     return url
--- src/plone/meta/shared/path.py
+++ src/plone/meta/shared/path.py
@@ -23,15 +23,17 @@ def path_factory(parameter_name, *, has_extension=None, is_dir=False):
 
     The factory raises an exception otherwise.
     """
+
     def factory(str):
         path = pathlib.Path(str)
         if not path.exists():
-            raise argparse.ArgumentTypeError(f'{str!r} does not exist!')
+            raise argparse.ArgumentTypeError(f"{str!r} does not exist!")
         if has_extension is not None and path.suffix != has_extension:
             raise argparse.ArgumentTypeError(
-                f'The required extension is {has_extension!r}'
-                f' not {path.suffix!r}.')
+                f"The required extension is {has_extension!r}" f" not {path.suffix!r}."
+            )
         if is_dir and not path.is_dir():
-            raise argparse.ArgumentTypeError('has to point to a directory!')
+            raise argparse.ArgumentTypeError("has to point to a directory!")
         return path
+
     return factory
